0232fce50eb68ecfced7429c5b1215f52b34e7aa
Vlad Petriv <vladpetriv05@gmail.com>
2024-03-06 21:52:33 +0100

    feat: impelment connection view

---
+++ web/src/views/ConnectionView.vue
@@ -0,0 +1,33 @@
+<script setup>
+import ConnectionForm from '@/components/ConnectionForm.vue';
+import ConnectionOption from '@/components/ConnectionOption.vue';
+import Header from '@/components/Header.vue';
+import Result from '@/components/Result.vue';
+import { ref } from 'vue';
+
+const connectionOptions = ref({
+  TestDatabaseConnection: false,
+  ConnectToDatabase: false,
+})
+</script>
+
+<template>
+  <Header />
+  <ConnectionOption :connectionOptions="connectionOptions" />
+  <div id="selector-result" class="result">
+    <h3 id="default-text" style="text-align: center;"
+      v-bind:style="[connectionOptions.TestDatabaseConnection || connectionOptions.ConnectToDatabase ? { 'display': 'none' } : { 'display': 'block' }]">
+      Please select an option before start.....
+    </h3>
+    <div class="container" id="container"
+      v-bind:style="[connectionOptions.TestDatabaseConnection || connectionOptions.ConnectToDatabase ? { 'display': 'flex' } : { 'display': 'none' }]">
+      <div class="form-container">
+        <h2 id="mode-title">
+          {{ connectionOptions.ConnectToDatabase ? 'Connect to the database' : 'Test database connection' }}
+        </h2>
+        <ConnectionForm :connectionOption="connectionOptions" :mode-title="modeTitle" />
+      </div>
+      <Result />
+    </div>
+  </div>
+</template>
