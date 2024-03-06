308ddb5e33d70fa4247b065ba7cb5996a4e06de9
Vlad Petriv <vladpetriv05@gmail.com>
2024-03-06 21:49:24 +0100

    feat: add Header and Footer components

---
+++ web/src/components/Footer.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+</script>
+
+<template>
+  <footer class="footer">
+    <div class="left-content">
+      <RouterLink to="/">
+        <img src="/src/assets/images/logo.png" alt="Logo">
+      </RouterLink>
+      <p>All rights reserved</p>
+    </div>
+    <div class="right-content">
+      <ul>
+        <li><a href="https://github.com/d2JSON">GitHub</a></li>
+        <li><a href="https://www.linkedin.com/in/vlad-petriv-855540223/">LinkedIn</a></li>
+        <li><a href="https://github.com/VladPetriv">Author GitHub</a></li>
+      </ul>
+    </div>
+  </footer>
+</template>
