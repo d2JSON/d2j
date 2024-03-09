f08bd81747c3f4f8e291fc7262318e9cd750490e
Vlad Petriv <vladpetriv05@gmail.com>
2024-03-09 21:10:54 +0100

    feat: implement interaction with server for test/connect db endpoints

--- web/src/views/ConnectionView.vue
+++ web/src/views/ConnectionView.vue
@@ -3,12 +3,75 @@ import ConnectionForm from '@/components/ConnectionForm.vue';
 import ConnectionOption from '@/components/ConnectionOption.vue';
 import Header from '@/components/Header.vue';
 import Result from '@/components/Result.vue';
+import router from '@/router';
+import { useAxios } from '@vueuse/integrations/useAxios';
 import { ref } from 'vue';
 
 const connectionOptions = ref({
   TestDatabaseConnection: false,
   ConnectToDatabase: false,
 })
+
+
+const loaderState = ref(false)
+const result = ref({})
+
+const handleTestConnection = async (connectionOptions) => {
+  loaderState.value = true
+
+  const { execute } =
+    useAxios('http://localhost:8080/api/database/test-connection',
+      { method: "POST" },
+      { immediate: false });
+
+  try {
+    const { data } = await execute({ data: connectionOptions })
+    result.value = data.value
+  } catch (err) {
+    result.value = {
+      message: err.response.data.message,
+    }
+  } finally {
+    loaderState.value = false
+  }
+}
+
+const handleConnectToDatabase = async (connectionOptions) => {
+  loaderState.value = true
+
+  const { execute } =
+    useAxios('http://localhost:8080/api/database/connect',
+      { method: "POST" },
+      { immediate: false });
+
+  try {
+    const { data } = await execute({
+      data: {
+        databaseConnectionOptions: connectionOptions,
+        connectionSessionTime: connectionOptions.connectionSessionTime,
+      }
+    })
+    saveDatabaseKeyToLocalStorage(data.value.databaseKey)
+    result.value = {
+      message: 'Successfully connected to your database!',
+    }
+
+    setTimeout(() => {
+      router.push({ name: "convert" })
+    }, 1000);
+
+  } catch (err) {
+    result.value = {
+      message: err.response.data.message,
+    }
+  } finally {
+    loaderState.value = false
+  }
+}
+
+const saveDatabaseKeyToLocalStorage = (databaseKey) => {
+  localStorage.setItem('databaseKey', databaseKey)
+}
 </script>
 
 <template>
@@ -25,9 +88,11 @@ const connectionOptions = ref({
         <h2 id="mode-title">
           {{ connectionOptions.ConnectToDatabase ? 'Connect to the database' : 'Test database connection' }}
         </h2>
-        <ConnectionForm :connectionOption="connectionOptions" :mode-title="modeTitle" />
+        <ConnectionForm :connectionOption="connectionOptions" @testDatabaseConnectionSubmitted="handleTestConnection"
+          @connectToDatabaseSubmitted="handleConnectToDatabase" />
       </div>
-      <Result />
+
+      <Result :result="result" :loader-state="loaderState" />
     </div>
   </div>
 </template>
